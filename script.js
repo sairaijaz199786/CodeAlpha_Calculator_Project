9b10b92a22306336083de62bdba9b94153d31383
sairaijaz199786 <sairaijaz199786@gmail.com>
2026-07-06 07:15:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let display = document.getElementById("display");
+
+function append(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown", function(e){
+
+    if((e.key >= "0" && e.key <= "9") || "+-*/.%".includes(e.key)){
+        display.value += e.key;
+    }
+
+    else if(e.key === "Enter"){
+        calculate();
+    }
+
+    else if(e.key === "Backspace"){
+        deleteLast();
+    }
+
+    else if(e.key === "Escape"){
+        clearDisplay();
+    }
+
+});
